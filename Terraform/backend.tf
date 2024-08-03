31d0023df1a7ea1c5cd4813917883d0ba7b20d63
Javier Palacios <javier.palacios@openintl.com>
2024-08-03 13:11:33 -0500

    backend

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,7 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "states-terraform"
-#     key    = "terraform.tfstate"
-#     region = var.region
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "states-terraform"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+    access_key = "${var.access_key}"
+    secret_key = "${var.secret_key}"
+  }
+}
