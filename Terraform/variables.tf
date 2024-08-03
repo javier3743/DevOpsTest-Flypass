07838afd1b0617e8bf5c66137baca41a964d76a5
Javier Palacios <javier.palacios@openintl.com>
2024-08-03 13:43:34 -0500

    fix: credentials

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -8,18 +8,6 @@ variable "username" {
   description = "The username tag for resources"
 }
 
-variable "access_key"{
-  description = "AWS access key"
-  type        = string
-  sensitive   = true
-}
-
-variable "secret_key"{
-  description = "AWS secret key"
-  type        = string
-  sensitive   = true
-}
-
 variable "cluster_roles" {
   description = "Cluster Admin Roles"
   type = list(string)
