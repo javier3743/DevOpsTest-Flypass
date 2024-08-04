e568eec461858574d9eb0b26d0e78447606b9ef8
Javier Palacios <javier.palacios@openintl.com>
2024-08-04 14:05:42 -0500

    Feat: count

--- Terraform/modules/vpc/main.tf
+++ Terraform/modules/vpc/main.tf
@@ -117,14 +117,14 @@ resource "aws_route" "nat" {
 #### Creating Route Table Association For Public & Private Subnets
 
 resource "aws_route_table_association" "public_rt" {
-  count = 2
-  subnet_id      = aws_subnet.eks_public_subnets[*].id
+  count = length(aws_subnet.eks_public_subnets)
+  subnet_id      = aws_subnet.eks_public_subnets[count.index].id
   route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_route_table_association" "private_rt" {
-  count = 2
-  subnet_id      = aws_subnet.eks_private_subnets[*].id
+  count = length(aws_subnet.eks_private_subnets)
+  subnet_id      = aws_subnet.eks_private_subnets[count.index].id
   route_table_id = aws_route_table.private_rt.id
 }
 
