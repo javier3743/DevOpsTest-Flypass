ddf04d5383d63c01d29e3aa2e406fc7d44061093
Javier Palacios <javier.palacios@openintl.com>
2024-08-04 03:52:50 -0500

    create 1

--- Terraform/modules/vpc/main.tf
+++ Terraform/modules/vpc/main.tf
@@ -30,7 +30,7 @@ resource "aws_subnet" "eks_public_subnet" {
   count = 2
   vpc_id = aws_vpc.vpc_eks.id
   cidr_block = element(var.public_subnet_cidr_block, count.index)
-  availability_zone = data.aws_availability_zones.available.names[0]
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
   map_public_ip_on_launch = true
   tags = {
     Name = "eks-public-subnet"
