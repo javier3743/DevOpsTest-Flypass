02158de7da214e75267e4f11d2ff1348814d5475
Javier Palacios <javier.palacios@openintl.com>
2024-08-04 10:23:06 -0500

    fix: index

--- Terraform/modules/vpc/main.tf
+++ Terraform/modules/vpc/main.tf
@@ -49,7 +49,7 @@ resource "aws_eip" "nat_eip" {
 resource "aws_nat_gateway" "eks_nat" {
   count = length(aws_subnet.eks_public_subnets.*.id)
   allocation_id = aws_eip.nat_eip[count.index].id
-  subnet_id     = aws_subnet.eks_public_subnets.id
+  subnet_id     = aws_subnet.eks_public_subnets.id[count.index]
 
   tags = {
     Name = "eks-nat-gateway"
